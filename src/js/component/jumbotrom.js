5db1cc161eb512d4ee849d54f62d3ec766b468dc
LaburgosCR <luis.larraburo92@gmail.com>
2021-02-08 20:00:32 +0000

    Landing page

---
+++ src/js/component/jumbotrom.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function Jumbotrom() {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-4">XBOX ONE GAMES</h1>
+			<i class="fab fa-xbox fa-5x"></i>
+			<p className="lead"></p>
+			<hr className="my-4" />
+			<p>
+				The largest catalog of games and entertainment No one has more
+				content for your Xbox, period. Get the latest releases,
+				blockbuster exclusives, season passes, add-on content, indie
+				games and more—all at great prices. Whether it’s a casual puzzle
+				game or the latest exclusive shooter that you’re looking for,
+				we’ve got games for every mood.
+			</p>
+			<a className="btn btn-primary btn-lg" href="#" role="button">
+				Learn more
+			</a>
+		</div>
+	);
+}
---
+++ src/js/component/nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Nav() {
+	return (
+		<nav className="navbar navbar-light bg-dark text-light">
+			<div className="container-fluid">
+				<span className="navbar-brand mb-0 h1"></span>
+			</div>
+		</nav>
+	);
+}
